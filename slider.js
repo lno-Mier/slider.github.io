5e1d3b964e62e8113e7475fb48e20070439205b6
lno-Mier <108462758+lno-Mier@users.noreply.github.com>
2022-10-13 16:25:46 +0600

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,15 +1,15 @@
 function slide() {
-	var left = 0
+	var left = '0'
 	console.log(left)
 	let button_right = document.getElementById('right')	
 	let button_left = document.getElementById('left')
 	let lenta = document.querySelector('.lenta')
 	button_left.onclick = function() {
-		left -= '500'
+		left =+ '500'
 		lenta.style.left = left + 'px'
 	}
 	button_right.onclick = function() {
-		left += '500'
+		left -= '500'
 		lenta.style.left = left + 'px'
 	}
 }
