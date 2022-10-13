7351217a8b42e6b1aa11970700b30345f9c27608
lno-Mier <108462758+lno-Mier@users.noreply.github.com>
2022-10-13 17:52:13 +0600

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,16 +1,22 @@
 function slide() {
-	var left = '0'
+	var left = 0
 	console.log(left)
 	let button_right = document.getElementById('right')	
 	let button_left = document.getElementById('left')
 	let lenta = document.querySelector('.lenta')
 	button_left.onclick = function() {
-		left =+ '500'
-		lenta.style.left = left + 'px'
+		left += 500
+		lenta.style.left = left + 'px'		
+		if (left > -500) {
+			left = -1500
+		}
 	}
 	button_right.onclick = function() {
-		left -= '500'
+		left -= 500
 		lenta.style.left = left + 'px'
+		if (left <= -1000) {
+			left = 500
+		}
 	}
 }
 slide()
